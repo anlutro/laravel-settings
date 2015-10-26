483bb2adec6fccb2ff7794d69c84af2d892643bf
Andreas Lutro <anlutro@gmail.com>
2015-10-26 18:08:46 +0100

    make migration compatible with more laravel versions
    fixes #33

--- src/migrations/2015_08_25_172600_create_settings_table.php
+++ src/migrations/2015_08_25_172600_create_settings_table.php
@@ -1,38 +1,42 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
 
 class CreateSettingsTable extends Migration
 {
-    public function __construct()
-    {
-        $this->tablename = config('settings.table');
-    }
+	public function __construct()
+	{
+		if (version_compare(Application::VERSION, '5.0', '>=')) {
+			$this->tablename = Config::get('settings.table');
+		} else {
+			$this->tablename = Config::get('anlutro/l4-settings::table');
+		}
+	}
 
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create($this->tablename, function(Blueprint $table)
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create($this->tablename, function(Blueprint $table)
 		{
-		    $table->increments('id');
-		    $table->string('key')->index();
-		    $table->text('value');
+			$table->increments('id');
+			$table->string('key')->index();
+			$table->text('value');
 		});
-    }
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop($this->tablename);
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop($this->tablename);
+	}
 }
-
