439e2fab348f237f7f3a70cbc8f23ff59b386412
mo7amed-3bdalla7 <mo7amed.3bdalla7@gmail.com>
2019-03-16 16:21:49 +0200

    add unique constraint to key column

---
+++ src/migrations/2019_03_16_141016_add_unique_to_key_column_in_settings_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Config;
+
+class AddUniqueToKeyColumnInSettingsTable extends Migration
+{
+    public function __construct()
+    {
+        if (version_compare(Application::VERSION, '5.0', '>=')) {
+            $this->tablename = Config::get('settings.table');
+            $this->keyColumn = Config::get('settings.keyColumn');
+            $this->valueColumn = Config::get('settings.valueColumn');
+        } else {
+            $this->tablename = Config::get('anlutro/l4-settings::table');
+            $this->keyColumn = Config::get('anlutro/l4-settings::keyColumn');
+            $this->valueColumn = Config::get('anlutro/l4-settings::valueColumn');
+        }
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->tablename, function (Blueprint $table) {
+            $table->dropIndex(Config::get('settings.table') . '_' . Config::get('settings.keyColumn') . '_index');
+            $table->unique($this->keyColumn, 'settings_key_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tablename, function (Blueprint $table) {
+            $table->dropUnique('settings_key_unique');
+            $table->Index(Config::get('settings.keyColumn'));
+        });
+    }
+}
+
