117e831f969a3c10a6c431121b27c30f078baa14
KailasRazorpay <kailas.sheregar@razorpay.com>
2020-06-11 21:41:28 +0530

    Removed commented line

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -59,7 +59,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 	public function boot()
 	{
 		if (version_compare(Application::VERSION, '5.3', '>=')) {
-//			$this->loadMigrationsFrom(__DIR__.'/migrations');
 			$this->publishes([
 				__DIR__.'/config/config.php' => config_path('settings.php')
 			], 'config');
