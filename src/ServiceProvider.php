b47b0abb1b8b2e96a461ee2f173144771b72cb53
sausin <sausin@users.noreply.github.com>
2021-07-26 15:33:23 +0530

    Update ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -11,134 +11,80 @@ namespace anlutro\LaravelSettings;
 
 use Illuminate\Foundation\Application;
 
-if (interface_exists('Illuminate\Contracts\Support\DeferrableProvider') {
-	class ServiceProvider extends \Illuminate\Support\ServiceProvider implements \Illuminate\Contracts\Support\DeferrableProvider
-	{
-		/**
-		 * Register IoC bindings.
-		 */
-		public function register()
-		{
-			// Bind the manager as a singleton on the container.
-			$this->app->singleton('anlutro\LaravelSettings\SettingsManager', function($app) {
-				/**
-				 * Construct the actual manager.
-				 */
-				return new SettingsManager($app);
-			});
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * This provider is deferred and should be lazy loaded.
+     *
+     * @var boolean
+     */
+    protected $defer = true;
 
-			// Provide a shortcut to the SettingStore for injecting into classes.
-			$this->app->bind('anlutro\LaravelSettings\SettingStore', function($app) {
-				return $app->make('anlutro\LaravelSettings\SettingsManager')->driver();
-			});
+    /**
+     * Register IoC bindings.
+     */
+    public function register()
+    {
+        $method = version_compare(Application::VERSION, '5.2', '>=') ? 'singleton' : 'bindShared';
 
-			$this->app->alias('anlutro\LaravelSettings\SettingStore', 'setting');
-			$this->mergeConfigFrom(__DIR__ . '/config/config.php', 'settings');
-		}
+        // Bind the manager as a singleton on the container.
+        $this->app->$method('anlutro\LaravelSettings\SettingsManager', function($app) {
+            // When the class has been resolved once, make sure that settings
+            // are saved when the application shuts down.
+            if (version_compare(Application::VERSION, '5.0', '<')) {
+                $app->shutdown(function($app) {
+                    $app->make('anlutro\LaravelSettings\SettingStore')->save();
+                });
+            }
 
-		/**
-		 * Boot the package.
-		 */
-		public function boot()
-		{
-			$this->publishes([
-				__DIR__.'/config/config.php' => config_path('settings.php')
-			], 'config');
-			$this->publishes([
-				__DIR__.'/migrations/2015_08_25_172600_create_settings_table.php' => database_path('migrations/'.date('Y_m_d_His').'_create_settings_table.php')
-			], 'migrations');
-		}
+            /**
+             * Construct the actual manager.
+             */
+            return new SettingsManager($app);
+        });
 
-		/**
-		 * Which IoC bindings the provider provides.
-		 *
-		 * @return array
-		 */
-		public function provides()
-		{
-			return array(
-				'anlutro\LaravelSettings\SettingsManager',
-				'anlutro\LaravelSettings\SettingStore',
-				'setting'
-			);
-		}
-	}
-} else {
-	class ServiceProvider extends \Illuminate\Support\ServiceProvider
-	{
-		/**
-		 * This provider is deferred and should be lazy loaded.
-		 *
-		 * @var boolean
-		 */
-		protected $defer = true;
+        // Provide a shortcut to the SettingStore for injecting into classes.
+        $this->app->bind('anlutro\LaravelSettings\SettingStore', function($app) {
+            return $app->make('anlutro\LaravelSettings\SettingsManager')->driver();
+        });
 
-		/**
-		 * Register IoC bindings.
-		 */
-		public function register()
-		{
-			$method = version_compare(Application::VERSION, '5.2', '>=') ? 'singleton' : 'bindShared';
+        $this->app->alias('anlutro\LaravelSettings\SettingStore', 'setting');
 
-			// Bind the manager as a singleton on the container.
-			$this->app->$method('anlutro\LaravelSettings\SettingsManager', function($app) {
-				// When the class has been resolved once, make sure that settings
-				// are saved when the application shuts down.
-				if (version_compare(Application::VERSION, '5.0', '<')) {
-					$app->shutdown(function($app) {
-						$app->make('anlutro\LaravelSettings\SettingStore')->save();
-					});
-				}
+        if (version_compare(Application::VERSION, '5.0', '>=')) {
+            $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'settings');
+        }
+    }
 
-				/**
-				 * Construct the actual manager.
-				 */
-				return new SettingsManager($app);
-			});
+    /**
+     * Boot the package.
+     */
+    public function boot()
+    {
+        if (version_compare(Application::VERSION, '5.0', '>=')) {
+            $this->publishes([
+                __DIR__.'/config/config.php' => config_path('settings.php')
+            ], 'config');
+            $this->publishes([
+                __DIR__.'/migrations/2015_08_25_172600_create_settings_table.php' => database_path('migrations/'.date('Y_m_d_His').'_create_settings_table.php')
+            ], 'migrations');
+        } else {
+            $this->app['config']->package(
+                'anlutro/l4-settings', __DIR__ . '/config', 'anlutro/l4-settings'
+            );
+        }
+    }
 
-			// Provide a shortcut to the SettingStore for injecting into classes.
-			$this->app->bind('anlutro\LaravelSettings\SettingStore', function($app) {
-				return $app->make('anlutro\LaravelSettings\SettingsManager')->driver();
-			});
-
-			$this->app->alias('anlutro\LaravelSettings\SettingStore', 'setting');
-
-			if (version_compare(Application::VERSION, '5.0', '>=')) {
-				$this->mergeConfigFrom(__DIR__ . '/config/config.php', 'settings');
-			}
-		}
-
-		/**
-		 * Boot the package.
-		 */
-		public function boot()
-		{
-			if (version_compare(Application::VERSION, '5.0', '>=')) {
-				$this->publishes([
-					__DIR__.'/config/config.php' => config_path('settings.php')
-				], 'config');
-				$this->publishes([
-					__DIR__.'/migrations/2015_08_25_172600_create_settings_table.php' => database_path('migrations/'.date('Y_m_d_His').'_create_settings_table.php')
-				], 'migrations');
-			} else {
-				$this->app['config']->package(
-					'anlutro/l4-settings', __DIR__ . '/config', 'anlutro/l4-settings'
-				);
-			}
-		}
-
-		/**
-		 * Which IoC bindings the provider provides.
-		 *
-		 * @return array
-		 */
-		public function provides()
-		{
-			return array(
-				'anlutro\LaravelSettings\SettingsManager',
-				'anlutro\LaravelSettings\SettingStore',
-				'setting'
-			);
-		}
-	}
+    /**
+     * Which IoC bindings the provider provides.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array(
+            'anlutro\LaravelSettings\SettingsManager',
+            'anlutro\LaravelSettings\SettingStore',
+            'setting'
+        );
+    }
 }
