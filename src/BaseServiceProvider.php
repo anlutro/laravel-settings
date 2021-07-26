2589b0b973acf26c1243fa67f0b88f963a9ff731
sausin <sausin@users.noreply.github.com>
2021-07-26 15:28:11 +0530

    Create BaseServiceProvider.php

---
+++ src/BaseServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Laravel 4 - Persistent Settings
+ * 
+ * @author   Andreas Lutro <anlutro@gmail.com>
+ * @license  http://opensource.org/licenses/MIT
+ * @package  l4-settings
+ */
+
+namespace anlutro\LaravelSettings;
+
+if (interface_exists('Illuminate\Contracts\Support\DeferrableProvider') {
+	class BaseServiceProvider extends \Illuminate\Support\ServiceProvider implements \Illuminate\Contracts\Support\DeferrableProvider
+	{
+    }
+} else {
+	class BaseServiceProvider extends \Illuminate\Support\ServiceProvider
+	{
+    }
+}
